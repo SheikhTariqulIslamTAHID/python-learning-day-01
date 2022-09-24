c492b0fcb35013dec57fcdfb3a3ec167506181f0
SK. Tariqul Islam Tahid <109989408+SheikhTariqulIslamTAHID@users.noreply.github.com>
2022-09-25 02:50:32 +0600

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,13 @@
+
+name=input("what is your name? ")
+print("Hi " + name)
+age=input("your age? ")
+print("great")
+birth_year = input("Enter your birth year: ")
+age= 2022 - int(birth_year)
+print(age)
+
+first=input("First number(int): ")
+second=input("Second number(float): ")
+sum=float(first)+float(second)
+print(sum)
